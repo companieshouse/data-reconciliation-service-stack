5120d110da89f7c141941d062e8070e268ad5c67
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2025-01-27 16:01:29 +0000

    modified code adding indentation and formatting

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -1,13 +1,14 @@
 terraform {
-  required_version = "~> 1.3"
+  backend "s3" {}
+  required_version = ">= 1.3, < 2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.54.0"
+      version = ">= 5.0, < 6.0"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.18.0"
+      version = ">= 4.0, < 5.0"
     }
   }
 }
@@ -16,26 +17,22 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "s3" {}
-}
-
 module "ecs-cluster" {
   source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-cluster?ref=1.0.297"
 
-  stack_name                 = local.stack_name
-  name_prefix                = local.name_prefix
-  environment                = var.environment
-  aws_profile                = var.aws_profile
-  vpc_id                     = data.aws_vpc.vpc.id
-  subnet_ids                 = local.application_subnet_ids
-  ec2_key_pair_name          = var.ec2_key_pair_name
-  ec2_instance_type          = var.ec2_instance_type
-  ec2_image_id               = var.ec2_image_id
-  asg_max_instance_count     = var.asg_max_instance_count
-  asg_min_instance_count     = var.asg_min_instance_count
-  enable_container_insights  = var.enable_container_insights
-  asg_desired_instance_count = var.asg_desired_instance_count
+  stack_name                  = local.stack_name
+  name_prefix                 = local.name_prefix
+  environment                 = var.environment
+  aws_profile                 = var.aws_profile
+  vpc_id                      = data.aws_vpc.vpc.id
+  subnet_ids                  = local.application_subnet_ids
+  ec2_key_pair_name           = var.ec2_key_pair_name
+  ec2_instance_type           = var.ec2_instance_type
+  ec2_image_id                = var.ec2_image_id
+  asg_max_instance_count      = var.asg_max_instance_count
+  asg_min_instance_count      = var.asg_min_instance_count
+  enable_container_insights   = var.enable_container_insights
+  asg_desired_instance_count  = var.asg_desired_instance_count
   scaledown_schedule          = var.asg_scaledown_schedule
   scaleup_schedule            = var.asg_scaleup_schedule
   enable_asg_autoscaling      = var.enable_asg_autoscaling
--- groups/infrastructure/variables.tf
+++ groups/infrastructure/variables.tf
@@ -1,72 +1,72 @@
 # Environment
 variable "environment" {
+  description = "The environment name, defined in envirionments vars."
   type        = string
-  description = "The environment name, defined in envrionments vars."
 }
 variable "aws_region" {
   default     = "eu-west-2"
-  type        = string
   description = "The AWS region for deployment."
+  type        = string
 }
 variable "aws_profile" {
   default     = "development-eu-west-2"
-  type        = string
   description = "The AWS profile to use for deployment."
+  type        = string
 }
 
 # EC2
 variable "ec2_key_pair_name" {
-  type        = string
   description = "The key pair for SSH access to ec2 instances in the clusters."
+  type        = string
 }
 variable "ec2_instance_type" {
   default     = "t3.medium"
-  type        = string
   description = "The instance type for ec2 instances in the clusters."
+  type        = string
 }
 variable "ec2_image_id" {
   default     = "ami-04018f95156d810bc" # ECS optimized Amazon2 Linux in London created 15/03/2023
-  type        = string
   description = "The machine image name for the ECS cluster launch configuration."
+  type        = string
 }
 
 # Auto-scaling Group
 variable "asg_max_instance_count" {
   default     = 0
-  type        = number
   description = "The maximum allowed number of instances in the autoscaling group for the cluster."
+  type        = number
 }
 variable "asg_min_instance_count" {
   default     = 0
-  type        = number
   description = "The minimum allowed number of instances in the autoscaling group for the cluster."
+  type        = number
 }
 variable "asg_desired_instance_count" {
   default     = 0
-  type        = number
   description = "The desired number of instances in the autoscaling group for the cluster. Must fall within the min/max instance count range."
+  type        = number
 }
 variable "asg_scaledown_schedule" {
   default     = ""
-  type        = string
   description = "The schedule to use when scaling down the number of EC2 instances to zero."
+  type        = string
 }
 
 variable "asg_scaleup_schedule" {
   default     = ""
-  type        = string
   description = "The schedule to use when scaling up the number of EC2 instances to their normal desired level."
+  type        = string
 }
 
 variable "enable_asg_autoscaling" {
   default     = true
-  type        = bool
   description = "Whether to enable auto-scaling of the ASG by creating a capacity provider for the ECS cluster."
+  type        = bool
 }
 
 # Container Insights - ECS
 variable "enable_container_insights" {
-  type        = bool
-  description = "A boolean value indicating whether to enable Container Insights or not"
   default     = true
+  description = "A boolean value indicating whether to enable Container Insights or not"
+  type        = bool
 }
