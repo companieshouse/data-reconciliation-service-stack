3080df0c2ab1362ff5df30f6219ea2e5ef585897
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2025-01-27 16:22:27 +0000

    modified aws version

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0, < 6.0"
+      version = ">= 4.54, < 5.0"
     }
     vault = {
       source  = "hashicorp/vault"
